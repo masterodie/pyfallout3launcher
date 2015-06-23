be5922602768107ac3d2b0dd2af5f1877c18c3bd
Patrick Neff <odie86@gmail.com>
2015-06-24 01:35:12 +0200

    Add config file

--- falloutlauncher.py
+++ falloutlauncher.py
@@ -16,6 +16,7 @@ limitations under the License.
 """
 
 import argparse
+import ConfigParser
 import logging
 import os
 import subprocess
@@ -31,11 +32,24 @@ except:
     sys.exit(1)
 
 FALLOUT_PATH = os.path.dirname(os.path.realpath(__file__))
-FALLOUT = ['Fallout 3', 'Fallout3.exe']
-FALLOUT_LAUNCHER = ['Launcher', 'FalloutLauncher_ORG.exe']
-FOSE = ['Fallout Script Extender', 'fose_loader.exe']
-MOD_ORGANIZER = ['Mod Organizer',
-                 os.path.join('ModOrganizer', 'ModOrganizer.exe')]
+CONFIG_FILE = 'pyFallout3Launcher.conf'
+
+
+def parse_configfile():
+    config = ConfigParser.ConfigParser()
+    if not os.path.exists(CONFIG_FILE):
+        config.add_section('Defaults')
+        config.set('Defaults', 'fo3_path', 'Fallout3.exe')
+        config.set('Defaults', 'launcher_path', 'FalloutLauncher_ORG.exe')
+        config.set('Defaults', 'fose_path', 'fose_loader.exe')
+        config.set('Defaults', 'mo_path',
+                   os.path.join('ModOrganizer', 'ModOrganizer.exe'))
+        config.set('Defaults', 'use_mo', 'yes')
+        config.set('Defaults', 'gui', 'yes')
+        with open(CONFIG_FILE, 'w') as configfile:
+            config.write(configfile)
+    config.read(CONFIG_FILE)
+    return config
 
 
 def str2bool(val):
@@ -43,6 +57,8 @@ def str2bool(val):
 
 
 def parse_arguments():
+    config = parse_configfile()
+    defaults = dict(config.items("Defaults"))
     parser = argparse.ArgumentParser(
         description="Steam Fallout 3 Launcher replacement"
     )
@@ -51,28 +67,37 @@ def parse_arguments():
 
     launcher_warning = \
         "(If launching through Mod Organizer this is only for configuration!)"
-    prog = parser.add_mutually_exclusive_group()
-    prog.add_argument(
+    apps = parser.add_argument_group('Launch Applications')
+    app = apps.add_mutually_exclusive_group()
+    app.add_argument(
         '--launcher',
         action='store_true',
-        help='Start Fallout 3 Launcher <{}> {}'.format(FALLOUT_LAUNCHER[1],
-                                                       launcher_warning),
+        help='Start Fallout 3 Launcher <{}> {}'.format(
+            config.get('Defaults', 'fo3_path'),
+            launcher_warning
+        ),
     )
-    prog.add_argument(
+    app.add_argument(
         '--fo3',
         action='store_true',
-        help='Start Fallout 3 <{}>'.format(FALLOUT[1]),
+        help='Start Fallout 3 <{}>'.format(
+            config.get('Defaults', 'launcher_path')
+        ),
     )
-    prog.add_argument(
+    app.add_argument(
         '--fose',
         action='store_true',
-        help='Start Fallout Script Extender <{}>'.format(FOSE[1]),
+        help='Start Fallout Script Extender'.format(
+            config.get('Defaults', 'fose_path')
+        ),
     )
-    prog.add_argument(
+    app.add_argument(
         '--mo',
         action='store_true',
-        help='Start ModOrganizer <{}>'.format(MOD_ORGANIZER[1])
+        help='Start ModOrganizer <{}>'.format(config.get('Defaults', 'mo_path'))
     )
+
+    parser.set_defaults(**defaults)
 
     modorganizer = parser.add_argument_group(
         "Mod Organizer Specific Options"
@@ -83,86 +108,77 @@ def parse_arguments():
         '--use-mo',
         help='Launch FOSE and Fallout through Mod Organizer',
         type='bool',
-        default=True,
+        default=config.getboolean('Defaults', 'use_mo'),
     )
 
     paths = parser.add_argument_group("Application Paths")
     paths.add_argument('--launcher-path',
                        help='Path to original FalloutLauncher.exe',
-                       default=FALLOUT_LAUNCHER[1])
+                       default=config.get('Defaults', 'launcher_path'))
     paths.add_argument('--fo3-path',
                        help='Path to Fallout.exe',
-                       default=FALLOUT[1])
+                       default=config.get('Defaults', 'fo3_path'))
     paths.add_argument('--fose-path',
                        help='Path to fose_loader.exe',
-                       default=FOSE[1])
+                       default=config.get('Defaults', 'fose_path'))
     paths.add_argument('--mo-path',
                        help='Path to ModOrganizer.exe',
-                       default=MOD_ORGANIZER[1])
+                       default=config.get('Defaults', 'mo_path'))
     paths.add_argument('--fallout-dir',
                        help='Path to Fallout 3 Directory',
                        default=FALLOUT_PATH)
 
     misc = parser.add_argument_group('Misc')
     misc.add_argument('--loglevel', help='Log level', default="WARNING")
+    misc.add_argument('--gui', dest='gui', action='store_true')
+    misc.add_argument('--no-gui', dest='gui', action='store_false')
+    misc.set_defaults(gui=config.getboolean('Defaults', 'gui'))
 
-    return parser
+    args = parser.parse_args()
+    return args
 
-parser = parse_arguments()
-args = parser.parse_args()
+args = parse_arguments()
 
-logging.basicConfig(filename='pyFallout3Launcher.log', level=getattr(logging,
-                                                                     args.loglevel.upper()))
-
-
-FALLOUT[1] = args.fo3_path
-FALLOUT_LAUNCHER[1] = args.launcher_path
-FOSE[1] = args.fose_path
-MOD_ORGANIZER[1] = args.mo_path
+logging.basicConfig(filename='pyFallout3Launcher.log',
+                    level=getattr(logging, args.loglevel))
 
 
 def mod_organizer(app):
-    drive, mo = os.path.splitdrive(MOD_ORGANIZER[1])
+    mo = args.mo_path
+    drive, mo = os.path.splitdrive(mo)
     if drive is None:
-        mo = os.path.join(FALLOUT_PATH, MOD_ORGANIZER[1])
-    drive, application = os.path.splitdrive(app[1])
+        mo = os.path.join(FALLOUT_PATH, mo)
+    drive, app = os.path.splitdrive(app)
     if drive is None:
-        application = os.path.join(FALLOUT_PATH, app[1])
-    retval = application
-    if os.path.exists(mo) and (app != MOD_ORGANIZER and
-                               app != FALLOUT_LAUNCHER):
+        application = os.path.join(FALLOUT_PATH, app)
+    retval = app
+    if os.path.exists(mo) and (app != args.mo_path and
+                               app != args.fo3_path):
             retval = (mo, '-p', args.profile, application)
-    loggig.debug(retval)
+    logging.debug(retval)
     return retval
 
 
 def run_app(app):
-    fallout = os.path.join(FALLOUT_PATH, FALLOUT[1])
+    fallout = os.path.join(FALLOUT_PATH, args.fo3_path)
     if not os.path.exists(fallout):
-        message = '''Fallout 3 not found!
-
-Please install this to your Fallout 3 directory!
-
-Installation procedure:
-    - Rename FalloutLauncher.exe to FalloutLauncher_ORG.exe
-    - Copy over falloutlauncher.exe your Fallout 3 directory
-    - Run through steam :)
-'''
-        logging.error('Fallout 3 Not found. Refer to README.md')
-
+        logging.error('{} Not found. Refer to README.md'.format(args.fo3_path))
         return 2
-    drive, path = os.path.splitdrive(app[1])
+
+    drive, path = os.path.splitdrive(app)
     if drive is None:
-        path = os.path.join(FALLOUT_PATH, app[1])
+        path = os.path.join(FALLOUT_PATH, app)
+
     if os.path.exists(path):
         if args.use_mo:
-            path = mod_organizer(app)
-        logging.info("Running {}".format(app[0]))
-        subprocess.Popen(path)
+            app = mod_organizer(app)
+        logging.info("Running {}".format(app))
+        subprocess.Popen(app)
         return 0
     else:
-        logging.error("{} executable <{}> does not exist!".format(app[0], path))
+        logging.error("{} does not exist!".format(app))
         return 1
+
 
 class GUI(tk.Frame):
     def __init__(self, master=None):
@@ -171,7 +187,7 @@ class GUI(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        top=self.winfo_toplevel()
+        top = self.winfo_toplevel()
         top.rowconfigure(0, weight=1)
         top.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
@@ -180,33 +196,33 @@ class GUI(tk.Frame):
         self.titleLabel = ttk.Label(
             self,
             text='Fallout 3 Launcher',
-            font = 'Helvetica 14 bold'
+            font='Helvetica 14 bold'
         )
         self.titleLabel.grid(pady=2, sticky=tk.N+tk.E+tk.W)
         self.fo3Button = ttk.Button(
             self,
-            text=FALLOUT[0],
+            text='Fallout 3',
             command=self.run_fo3,
             width=25
         )
         self.fo3Button.grid(pady=2, sticky=tk.N+tk.E+tk.W)
         self.launcherButton = ttk.Button(
             self,
-            text=FALLOUT_LAUNCHER[0],
+            text='Fallout 3 Launcher',
             command=self.run_launcher,
             width=25
         )
         self.launcherButton.grid(pady=2, sticky=tk.N+tk.E+tk.W)
         self.foseButton = ttk.Button(
             self,
-            text=FOSE[0],
+            text='Fallout Script Extender',
             command=self.run_fose,
             width=25
         )
         self.foseButton.grid(pady=2, sticky=tk.N+tk.E+tk.W)
         self.moButton = ttk.Button(
             self,
-            text=MOD_ORGANIZER[0],
+            text='Mod Organizer',
             command=self.run_mo,
             width=25
         )
@@ -220,16 +236,16 @@ class GUI(tk.Frame):
         self.quitButton.grid(pady=2, sticky=tk.S+tk.E+tk.W)
 
     def run_fo3(self):
-        run_app(FALLOUT)
+        run_app(args.fo3_path)
 
     def run_launcher(self):
-        run_app(FALLOUT_LAUNCHER)
+        run_app(args.launcher_path)
 
     def run_fose(self):
-        run_app(FOSE)
+        run_app(args.fose_path)
 
     def run_mo(self):
-        run_app(MOD_ORGANIZER)
+        run_app(args.mo_path)
 
 
 def user_input():
@@ -238,44 +254,45 @@ def user_input():
         launcher_warning = "(Only for configuration!)"
     print "Fallout 3 Launcher replacement"
     print
-    print "<1>\tStart Fallout 3\t\t\t<{}>".format(FALLOUT[1])
-    print "<2>\tStart Fallout 3 Launcher\t<{}> {}".format(FALLOUT_LAUNCHER[1],
+    print "<1>\tStart Fallout 3\t\t\t<{}>".format(args.fo3_path)
+    print "<2>\tStart Fallout 3 Launcher\t<{}> {}".format(args.launcher_path,
                                                           launcher_warning)
-    print "<3>\tStart Fallout Script Extender\t<{}>".format(FOSE[1])
-    print "<4>\tStart Mod Organizer\t\t<{}>".format(MOD_ORGANIZER[1])
+    print "<3>\tStart Fallout Script Extender\t<{}>".format(args.fose_path)
+    print "<4>\tStart Mod Organizer\t\t<{}>".format(args.mo_path)
     print "<ESC>\tQuit"
     print
 
     while True:
         choice = getch()
         if choice == "1":
-            run_app(FALLOUT)
+            run_app(args.fo3_path)
         elif choice == "2":
-            run_app(FALLOUT_LAUNCHER)
+            run_app(args.launcher_path)
         elif choice == "3":
-            run_app(FOSE)
+            run_app(args.fose_path)
         elif choice == "4":
-            run_app(MOD_ORGANIZER)
+            run_app(args.mo_path)
         elif choice == chr(27):
             sys.exit(0)
 
 
 def main():
 
-    #if args.fo3:
-        #run_app(FALLOUT)
-    #elif args.launcher:
-        #run_app(FALLOUT_LAUNCHER)
-    #elif args.fose:
-        #run_app(FOSE)
-    #elif args.mo:
-        #run_app(MOD_ORGANIZER)
-    #else:
-        #user_input()
-
-    app = GUI()
-    app.master.title('Fallout 3 Launcher')
-    app.mainloop()
+    if args.fo3:
+        run_app(args.fo3_path)
+    elif args.launcher:
+        run_app(args.launcher_path)
+    elif args.fose:
+        run_app(args.fose_path)
+    elif args.mo:
+        run_app(args.mo_path)
+    else:
+        if args.gui is False:
+            user_input()
+        else:
+            app = GUI()
+            app.master.title('Alternative Fallout 3 Launcher')
+            app.mainloop()
 
 
 if __name__ == '__main__':
